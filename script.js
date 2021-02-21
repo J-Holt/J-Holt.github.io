b7f3489cd2fbe7c444d48a3d7b83c3b21a7451e9
J-Holt <79391081+J-Holt@users.noreply.github.com>
2021-02-21 00:32:56 -0500

    Updated code.

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function luckyNumbers() {
     let play = [];
     for (l=0;l < 5;l++) { // Loop 5 times to generate 5 random numbers
       let num = Math.round(Math.random() * 70).toString(); // Random number generator
-      if (num.toString().length == 1) {num = "0" + num;}; // Check if number is only 1 digit and add a zero prefix if so
+      if (num == 0) {num = Math.round(Math.random() * 70).toString();}
+      if (num.toString().length == 1) {num = "0" + num}; // Check if number is only 1 digit and add a zero prefix if so
         play.push((`<span class="regNums">`+ num +`</span>`)); // Create a span element with the random number
     } // End of nested for loop
   
     let powerball = Math.round(Math.random() * 25).toString();
+    if (powerball == 0) {powerball = Math.round(Math.random() * 70).toString();}
+    if (powerball.toString().length == 1) {powerball = "0" + powerball;}; 
     play.push((`<span class="powerNums">`+ powerball +`</span>`));  
     let j = play.join(' ');
     plays.push(j);
